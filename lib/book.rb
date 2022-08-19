7b2a234586ca075c93df4518fe15d65b7ce59359
Alex Mwaura <alexmwaura36@gmail.com>
2022-08-19 13:44:20 +0300

    commit tree 4f8da40ad58a6252b4b29518169bf5ce851ec169

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,23 @@
+class Book
+    attr_accessor :title, :author, :page_count, :genre
+    def initialize(title)
+        @title = title
+    
+    end
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
 
+    end
+end
+book = Book.new("red pil awakening")
+book.title
+puts book.title
+book.author ="andrew tate"
+book.author
+puts book.author
+book.page_count = 70
+book.page_count
+puts book.page_count
+book.genre = "wellness of men"
+puts book.genre
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,18 @@
 # Make your shoe class here!
+class Shoe
+    attr_accessor :brand, :size, :color, :material, :condition
+    def initialize(brand)
+        @brand = brand
+    end
+    def cobble
+        @condition = "new"
+        puts "Your shoe is as good as new!"
+
+    end
+end
+shoe = Shoe.new("yeezy")
+shoe.brand
+puts shoe.brand
+shoe.size = 41
+shoe.size
+puts shoe.size
